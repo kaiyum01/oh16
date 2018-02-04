fffb245675989f1f6a61a6e9b4208c89dbda34f2
kaiyum <kaiyum@logicsoftld.com>
2018-02-04 11:57:41 +0600

    updated

--- include/db_connection.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Establishing connection with server..
-  $connection = mysql_connect("abdulkaiyum.com", "kaiyum123", "X42e77HHOmikAH");
-
-// Selecting Database 
-  $db = mysql_select_db("kaiyum12_oh16", $connection);
-  ?>
