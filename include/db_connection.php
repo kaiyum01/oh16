4868c8a17ad555d02923739b3d5edac40591aeb1
Abdul Kaiyum <ma.kaiyum1992@gmail.com>
2018-02-05 17:00:21 +0600

    Update db_connection.php

--- include/db_connection.php
+++ include/db_connection.php
@@ -1,6 +1,6 @@
 <?php
 // Establishing connection with server..
-  $connection = mysql_connect("abdulkaiyum.com", "userName", "passWord");
+  $connection = mysql_connect("localhost", "userName", "passWord");
 
 // Selecting Database 
   $db = mysql_select_db("DBname", $connection);
